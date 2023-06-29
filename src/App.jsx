f659d66181028756d6e9c76a56f28784aa6e8719
Abraham Cabrera <ingeabraham23@gmail.com>
2023-06-29 08:00:01 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import es from 'date-fns/locale/es'
+import DatePicker from 'react-datepicker';
+import { registerLocale } from  "react-datepicker"
+import 'react-datepicker/dist/react-datepicker.css';
+import Tabla from './components/tabla';
+import './estilos.css'
+import EditableTabla from './components/tablaEditable';
+
+registerLocale('es', es)
+
+function App() {
+  const fechaActual = new Date(); // Obtiene la fecha actual
+  const diaCero = new Date(2023, 3, 26); // Fecha inicial para rotar los grupos
+  const [fechaSeleccionada, setFechaSeleccionada] = useState(fechaActual); // Estado para la fecha seleccionada en el DatePicker, se inicializa con la fecha en que se esta ejecutando la aplicación
+
+  const [grupos, setGrupos] = useState([
+    { nombre: 'Humberto', numeros: [7, 8, 34, 42] },
+    { nombre: 'Marce', numeros: [6, 14, 22, 29, 41] },
+    { nombre: 'Paco', numeros: [5, 13, 21, 33, 39] },
+    { nombre: 'Heriberto', numeros: [4, 12, 20, 31] },
+    { nombre: 'Lobo', numeros: [11, 19, 28, 38] },
+    { nombre: 'David', numeros: [10, 18, 27, 37, 47] },
+    { nombre: 'Eloy', numeros: [1, 9, 25, 36, 46] },
+    { nombre: 'Hector', numeros: [16, 24, 35, 43] },
+  ]);// Estado para los grupos de Unidades
+
+  const gruposRotados = rotarGrupos();
+
+  function obtenerDiasDeDiferencia(fechaSeleccionada, fechaDiaCero) {
+    const unDia = 24 * 60 * 60 * 1000; // milisegundos en un día: 86,400,000
+    const diffEnMilisegundos = (fechaSeleccionada - fechaDiaCero); //Diferencia de 2 Fechas en Milisegundos
+    const dias = Math.floor(diffEnMilisegundos / unDia); //Diferencia de 2 fechas en Días, Usando Math.floor para redondear al piso, es decir, si el resultado es 8.36 se redondea a 8 ya que la fecha diaCero esta inicializada a las 0 horas y si yo hago el calculo a las 9 horas ya habra una diferencia de 9 horas
+    return dias
+  }// Obtiene el número de días entre dos fechas
+
+  function rotarGrupos() {
+    const diasTranscurridos = obtenerDiasDeDiferencia(fechaSeleccionada, diaCero);
+    const rotaciones = (diasTranscurridos) % grupos.length; //Se obtiene el numero de rotaciones:Obteniendo el Modulo de: dias transcurridos, entre en tamaño del array grupos. 
+    const gruposRotados = [...grupos.slice(-rotaciones), ...grupos.slice(0, -rotaciones)];
+    return gruposRotados;
+  }
+
+  function manejarFechaSeleccionada(fecha) {
+    setFechaSeleccionada(fecha);
+  }
+
+  function manejarCambioEnGrupos(nuevosGrupos) {
+    setGrupos(nuevosGrupos);
+  }
+
+  return (
+    <div>
+      <EditableTabla grupos={grupos} onGruposChange={manejarCambioEnGrupos}></EditableTabla>
+      <DatePicker selected={fechaSeleccionada} onChange={manejarFechaSeleccionada} locale="es" onFocus={(e) => { e.target.readOnly = true; e.target.blur() }}/>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Tabla grupos={gruposRotados} fecha={fechaSeleccionada} />
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/tabla.jsx
@@ -0,0 +1,212 @@
+/* eslint-disable react/prop-types */
+import React, { useRef } from "react";
+import html2canvas from "html2canvas";
+
+function Tabla({ grupos, fecha }) {
+  const opciones = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const coloresFila = [
+    "#EA17FC",
+    "#1668E8",
+    "#4AEC09",
+    "#F6F90C",
+    "#B4B4B2",
+    "#4AEC09",
+    "#F9AA01",
+    "#EE4040",
+  ];
+  const rutas = [
+    "Sosa",
+    "Calicapan",
+    "Coahuixco",
+    "Tacopan",
+    "San Isidro",
+    "Coahuixco",
+    "Tezotepec",
+    "San Miguel",
+  ];
+  const tablaRef = useRef(null);
+
+  function capturarTabla() {
+    const tabla = tablaRef.current;
+    html2canvas(tabla).then(function (canvas) {
+      const pngUrl = canvas.toDataURL("image/png");
+      const downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = fecha.toLocaleDateString("es-MX", opciones);
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    });
+  }
+
+  return (
+    <div>
+      <table align="center" ref={tablaRef}>
+        <thead>
+          <tr>
+            <th colSpan={5} style={{ backgroundColor: "#B4B4B2" }}>
+              {fecha.toLocaleDateString("es-MX", opciones)}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {grupos.map((grupo, index) => {
+            if (index === 3) {
+              return (
+                <React.Fragment key={index}>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      7:15, 8:15, 9:15
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[0] === 0
+                        ? " "
+                        : grupos[index].numeros[0]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      6:30, 7:30, 8:30
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[1] === 0
+                        ? " "
+                        : grupos[index].numeros[1]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      6:45, 7:45, 8:45
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[2] === 0
+                        ? " "
+                        : grupos[index].numeros[2]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      7:00, 8:00, 9:00
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[3] === 0
+                        ? " "
+                        : grupos[index].numeros[3]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td></td>
+                    <td colSpan={2} className="celda-tacopan">
+                      Descansa:{" "}
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[4]}
+                    </td>
+                    <td></td>
+                  </tr>
+                </React.Fragment>
+              );
+            } else if (index === 7) {
+              return (
+                <React.Fragment key={index}>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      7:05
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[0] === 0
+                        ? " "
+                        : grupos[index].numeros[0]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      7:15
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[1] === 0
+                        ? " "
+                        : grupos[index].numeros[1]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      7:25
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[2] === 0
+                        ? " "
+                        : grupos[index].numeros[2]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td className="celda-tacopan">{rutas[index]}</td>
+                    <td colSpan={2} className="celda-tacopan">
+                      7:35
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[3] === 0
+                        ? " "
+                        : grupos[index].numeros[3]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td></td>
+                    <td colSpan={2} className="celda-tacopan">
+                      Descansa:{" "}
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[4]}
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr style={{ backgroundColor: coloresFila[index] }}>
+                    <td></td>
+                    <td colSpan={2} className="celda-tacopan">
+                      Descansa:{" "}
+                    </td>
+                    <td className="celda-tacopan">
+                      {grupos[index].numeros[5]}
+                    </td>
+                    <td></td>
+                  </tr>
+                </React.Fragment>
+              );
+            } else {
+              return (
+                <tr key={index} style={{ backgroundColor: coloresFila[index] }}>
+                  <td>{rutas[index]}</td>
+                  <td colSpan={3}>{grupo.numeros.join(", ")}</td>
+                  <td>{grupo.nombre}</td>
+                </tr>
+              );
+            }
+          })}
+        </tbody>
+      </table>
+      <button onClick={capturarTabla}>Capturar tabla</button>
+    </div>
+  );
+}
+
+export default Tabla;
