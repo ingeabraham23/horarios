e1a5642b8a87a3094401abc53fe103b829bb7de5
Abraham Cabrera <ingeabraham23@gmail.com>
2024-01-26 13:16:37 -0600

    movimiento 3 y 37

--- src/components/Secundaria.jsx
+++ src/components/Secundaria.jsx
@@ -16,11 +16,11 @@ function Secundaria() {
   const [fechaSeleccionada, setFechaSeleccionada] = useState(fechaActual); // Estado para la fecha seleccionada en el DatePicker, se inicializa con la fecha en que se esta ejecutando la aplicación
 
   const [grupos, setGrupos] = useState([
-    { nombre: "GRUPO 7", numeros: [7, 34, 37, 42, 8, 15, 23] },
+    { nombre: "GRUPO 7", numeros: [3, 7, 34, 42, 8, 15, 23] },
     { nombre: "GRUPO 6", numeros: [6, 14, 22, 29, 41] },
     { nombre: "GRUPO 5", numeros: [13, 19, 21, 33, 39, 5, 40] },
     { nombre: "GRUPO 4", numeros: [4, 12, 20, 31] },
-    { nombre: "GRUPO 3", numeros: [3, 11, 28, 38] },
+    { nombre: "GRUPO 3", numeros: [11, 28, 37, 38] },
     { nombre: "GRUPO 2", numeros: [10, 18, 27, 47, 2] },
     { nombre: "GRUPO 1", numeros: [1, 9, 25, 36, 46, 30, 17] },
     { nombre: "GRUPO 8", numeros: [16, 24, 35, 43] },
